684d838ee7b121b2812fbae2596b04014610b3d5
dabaitudiu <dabaitudiu@163.com>
2019-01-11 20:35:35 +0800

    rectify errors, split data to train,val,test

--- rssi_dnn_keras.py
+++ rssi_dnn_keras.py
@@ -5,10 +5,16 @@ import tensorflow as tf
 
 x,y = data.read_datasets()
 train_val_split = int(0.7 * len(x))  # mask index array
+val_test_split = int(0.8 * len(x))
+# train
 x_train = np.array(x[:train_val_split])
 y_train = np.array(y[:train_val_split])
-x_val = np.array(x[train_val_split:])
-y_val = np.array(y[train_val_split:])
+# validation
+x_val = np.array(x[train_val_split:val_test_split])
+y_val = np.array(y[train_val_split:val_test_split])
+# test
+x_test = np.array(x[val_test_split:])
+y_test = np.array(y[val_test_split:])
 
 
 print("data shapes:")
@@ -31,40 +37,43 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=20)
+history = model.fit(x_train, y_train, epochs=20,batch_size=512,validation_data=(x_val,y_val))
 
-test_loss, test_acc = model.evaluate(x_val,y_val)
+test_loss, test_acc = model.evaluate(x_test,y_test)
 
 print('Test accuracy:', test_acc)
 
-predictions = model.predict(x_val)
+predictions = model.predict(x_test)
 
-diff_building = 0
-diff_floor = 0
-diff_place = 0
+b = 0
+b_f = 0
+b_f_p = 0
 
-for i in range(1,len(x_val)):
+for i in range(1,len(x_test)):
     sub1 = np.argmax(predictions[-i][:3])
     sub2 = np.argmax(predictions[-i][3:8])
     sub3 = np.argmax(predictions[-i][8:118])
 
-    sub4 = np.argmax(y_val[-i][:3])
-    sub5 = np.argmax(y_val[-i][3:8])
-    sub6 = np.argmax(y_val[-i][8:118])
+    sub4 = np.argmax(y_test[-i][:3])
+    sub5 = np.argmax(y_test[-i][3:8])
+    sub6 = np.argmax(y_test[-i][8:118])
 
-    if (sub1 != sub4):
-        diff_building += 1
+    if (sub1 == sub4) and (sub2 == sub5) and (sub3 == sub6):
+        b_f_p += 1
+        b_f += 1
+        b += 1
     else:
-        if (sub2 != sub5):
-            diff_floor += 1
+        if (sub1 == sub4) and (sub2 == sub5):
+            b_f += 1
+            b += 1
         else:
-            if (sub3 != sub6):
-                diff_place += 1
+            if (sub1 == sub4):
+                b += 1
 
-s = len(x_val)
-a1 = 100 * (s - diff_building) / s
-a2 = 100 * (s - diff_floor) / s
-a3 = 100 * (s - diff_place) / s
+s = len(x_test)
+a1 = 100 * b / s
+a2 = 100 * b_f / s
+a3 = 100 * b_f_p / s
 
 print("For ",s," test data:")
 print("building accuracy: ", a1,"%")
