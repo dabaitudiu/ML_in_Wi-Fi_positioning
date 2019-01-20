f7490906b577a7c8680d36ff3a30d8a7dee0d52c
dabaitudiu <bigbroase@163.com>
2019-01-20 11:43:56 +0800

    Reorgranized manual buildings, suitable for different models

--- Optimized_Manual_Buildings.py
+++ Optimized_Manual_Buildings.py
@@ -4,98 +4,106 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras.utils.np_utils import to_categorical
+import pandas as pd
 
-fpath = "MNIST_data/test/datav2.xlsx"
-fpath2 = "MNIST_data/test/validationData2.xlsx"
+fpath1 = "trainingData2.csv"
+fpath2 = "validationData2.csv"
 
-filepath = fpath
-xl_book = xlrd.open_workbook(filepath)
-xl_table = xl_book.sheets()[0]
-xl_length = xl_table.nrows
-
-filepath2 = fpath2
-xl_book2 = xlrd.open_workbook(filepath2)
-xl_table2 = xl_book2.sheets()[0]
-xl_length2 = xl_table2.nrows
-
-# training data
-x = []
-# lables
-y_ = []
+MODEL_NAME = "DNN"
 
 
-# test data
-x_test = []
-# lables
-y_test = []
+def read_data(fpath, method):
+    train_df = pd.read_csv(fpath, header=0)
+    data_len = len(train_df)
+
+    buildings = {}
+    building_labels = {}
+
+    idx = np.arange(data_len)
+    np.random.shuffle(idx)
+    rows = np.asarray(train_df.iloc[:, :]).astype(float)
+
+    for i in idx:
+        row = rows[i]
+        building_number = int(float(row[523]))
+        if building_number not in buildings.keys():
+            buildings[building_number] = []
+            building_labels[building_number] = []
+        if method == "CNN":
+            sub_x = (row[:520].astype(float) + 110) * 255 / 110
+            ax =[np.zeros(9)]
+            sub_row = np.append(sub_x, ax)
+            sub_row = sub_row.reshape(23, 23, 1)
+        else:
+            if method == "DNN":
+                sub_row = (row[:520].astype(float) + 110) / 110
+            else:
+                raise ValueError('Model unspecified.')
+        buildings[building_number].append(sub_row)
+        label = to_categorical(int(float(row[522])), num_classes=5)
+        building_labels[building_number].append(label)
+    return buildings, building_labels
 
 
-building_signals = [[], [], []]
-building_labels = [[], [], []]
+def nn_model(model_name):
+    if model_name == "DNN":
+        model = keras.Sequential()
+        model.add(keras.layers.Dense(256, activation='relu', input_shape=(520,)))
+        model.add(keras.layers.Dropout(0.2))
+        model.add(keras.layers.Dense(128, activation='relu'))
+        model.add(keras.layers.Dropout(0.2))
+        model.add(keras.layers.Dense(64, activation='relu'))
+        model.add(keras.layers.Dropout(0.2))
+        model.add(keras.layers.Dense(32, activation='relu'))
+        model.add(keras.layers.Dropout(0.2))
+        model.add(keras.layers.Dense(16, activation='relu'))
+        model.add(keras.layers.Dropout(0.2))
+        model.add(keras.layers.Dense(5, activation='softmax'))
 
-building_signals2 = [[], [], []]
-building_labels2 = [[], [], []]
+        model.compile(optimizer='rmsprop',
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy'])
+    if model_name == "CNN":
+        model = models.Sequential()
+        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(23, 23, 1)))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
 
-for i in range(1, xl_length):
-    row = np.array(xl_table.row_values(i))
-    building_number = int(float(row[523]))
-    building_signals[building_number].append((row[:520].astype(np.float64) + 110) / 110)
-    label = to_categorical(int(float(row[522])), num_classes=5)
-    building_labels[building_number].append(label)
+        model.add(layers.Flatten())
+        model.add(layers.Dense(256, activation='relu'))
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dropout(0.2))
+        model.add(layers.Dense(8, activation='sigmoid'))
 
-for i in range(3):
-    building_signals[i] = np.array(building_signals[i])
-    building_labels[i] = np.array(building_labels[i])
-    print(building_signals[i].shape)
-    print(building_labels[i].shape)
+        model.compile(optimizer=tf.train.AdamOptimizer(),
+                      loss='binary_crossentropy',
+                      metrics=['accuracy'])
+    return model
 
-for i in range(1, xl_length2):
-    row2 = np.array(xl_table2.row_values(i))
-    building_number2 = int(float(row2[523]))
-    building_signals2[building_number2].append((row2[:520].astype(np.float64) + 110) / 110)
-    label2 = to_categorical(int(float(row2[522])), num_classes=5)
-    building_labels2[building_number2].append(label2)
 
-for i in range(3):
-    building_signals2[i] = np.array(building_signals2[i])
-    building_labels2[i] = np.array(building_labels2[i])
-    print(building_signals2[i].shape)
-    print(building_labels2[i].shape)
+train_buildings, train_labels = read_data(fpath1, MODEL_NAME)
+test_buildings, test_labels = read_data(fpath2, MODEL_NAME)
+for key in train_buildings.keys():
+    print("Group ", key, " : ")
 
-# train buildings: --------------------------------------------------------------------------------------------------------------------------------------------------
-for i in range(3):
-    print("Building ",i, " Starts Training:")
-    x = building_signals[i]
-    y = building_labels[i]
-
-    x_test = np.array(building_signals2[i])
-    y_test = np.array(building_labels2[i])
-
+    x = train_buildings[key]
+    y = train_labels[key]
     train_val_split = int(0.9 * len(x))  # mask index array
-
     # train
     x_train = np.array(x[:train_val_split])
     y_train = np.array(y[:train_val_split])
     # validation
     x_val = np.array(x[train_val_split:])
     y_val = np.array(y[train_val_split:])
+    # test
+    x_test = np.array(test_buildings[key])
+    y_test = np.array(test_labels[key])
 
-    model = keras.Sequential()
-    model.add(keras.layers.Dense(256, activation='relu', input_shape=(520,)))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(128, activation='relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(64, activation='relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(32, activation='relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(16, activation='relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(5, activation='softmax'))
-
-    model.compile(optimizer='rmsprop',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model = nn_model(MODEL_NAME)
 
     history = model.fit(x_train, y_train, epochs=20, batch_size=512, validation_data=(x_val, y_val))
 
@@ -150,6 +158,4 @@ for i in range(3):
     plt.legend()
 
     plt.show()
-
-
 
