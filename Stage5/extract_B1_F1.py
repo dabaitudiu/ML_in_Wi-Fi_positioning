e2ffba7310c1d7b23306aaaeddcde268b5478352
dabaitudiu <bigbroase@163.com>
2019-01-23 22:46:15 +0800

    correct errors in extract_B1_F1.py

--- Stage5/extract_B1_F1.py
+++ Stage5/extract_B1_F1.py
@@ -13,17 +13,24 @@ from keras import optimizers
 from keras.applications import VGG16 
 from keras.applications import InceptionV3
 import scipy.stats
+import os 
 
 
 fpath1 = "trainingData2.csv"
 fpath2 = "validationData2.csv"
 
 MODEL_NAME = "DNN"
+INPUT_DIM = 520
+
+save_dir = 'MNIST_data/extract_B1F1/'
+if os.path.exists(save_dir) is False:
+    os.makedirs(save_dir)
 
 # train_df = pd.read_csv(fpath1, header=0)
 # sub_rows = np.asarray(train_df.iloc[:, 0:520]).astype(float)
-# sel = VarianceThreshold(threshold=(.9 * (1 - .9)))
+# sel = VarianceThreshold(threshold=0)
 # sel.fit(sub_rows)
+
 sub_floors = [[],[],[],[]]
 
 def read_data(fpath, method):
@@ -38,9 +45,8 @@ def read_data(fpath, method):
     rows = np.asarray(train_df.iloc[:, :]).astype(float)
     sub_rows = np.asarray(train_df.iloc[:, 0:520]).astype(float)
     # sub_rows = sel.transform(sub_rows)
-    print(sub_rows.shape)
     
-
+    ind_k = 0
 
     for i in idx:
         row = rows[i]
@@ -54,17 +60,24 @@ def read_data(fpath, method):
             sub_row = np.append(sub_x, ax)
             sub_row = sub_row.reshape(23, 23, 1)
         if method == "DNN":
-            sub_row = sub_rows[i].astype(float)
-            if building_number == 1:
+            sub_row = (sub_rows[i].astype(float) + 110) / 110
+            if building_number == 1 and fpath == "validationData2.csv":
                 tmp_label = int(float(row[522]))
                 sub_floors[tmp_label].append(sub_row)
-        if method == "VGG16":
+                if tmp_label == 1:
+                    ind_k += 1
+                    ax = [np.zeros(9)]
+                    concax = np.append(sub_row,ax)
+                    concax = concax.reshape(23,23)
+                    filename = save_dir + '%d.jpg'%ind_k
+                    scipy.misc.toimage(concax, cmin=0.0, cmax=1.0).save(filename)
+        if method == "VGG16" or method =="S_CNN":
             sub_x = (sub_rows[i].astype(float) + 110) / 110
             sub_row = np.append(sub_x, sub_x)[:1024]
             sub_row = sub_row.reshape(32, 32, 1)
             tmp = np.append(sub_row, sub_row,axis=2)
             sub_row = np.append(tmp, sub_row,axis=2)
-        if method != "CNN" and method != "DNN" and method != "VGG16":
+        if method != "CNN" and method != "DNN" and method != "VGG16" and method != "S_CNN":
             raise ValueError('Model unspecified.')
         buildings[building_number].append(sub_row)
         label = to_categorical(int(float(row[522])), num_classes=5)
@@ -75,7 +88,7 @@ def read_data(fpath, method):
 def nn_model(model_name):
     if model_name == "DNN":
         model = keras.Sequential()
-        model.add(keras.layers.Dense(256, activation='relu', input_shape=(520,)))
+        model.add(keras.layers.Dense(256, activation='relu', input_shape=(INPUT_DIM,)))
         model.add(keras.layers.Dropout(0.2))
         model.add(keras.layers.Dense(128, activation='relu'))
         model.add(keras.layers.Dropout(0.2))
@@ -92,7 +105,7 @@ def nn_model(model_name):
                       metrics=['accuracy'])
     if model_name == "CNN":
         model = models.Sequential()
-        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(23, 23, 1)))
+        model.add(layers.Conv2D(32, 3, activation='relu', input_shape=(23, 23, 1)))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
@@ -139,34 +152,56 @@ def nn_model(model_name):
         model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
               loss='binary_crossentropy',
               metrics=['acc'])
+
+    if model_name == "S_CNN":
+        model = models.Sequential()
+        model.add(layers.SeparableConv2D(32,3,activation='relu',input_shape=(32,32,3)))
+        model.add(layers.SeparableConv2D(64,3,activation='relu'))
+        model.add(layers.MaxPooling2D(2))
+        
+        model.add(layers.SeparableConv2D(64,3,activation='relu'))
+        model.add(layers.SeparableConv2D(128,3,activation='relu'))
+        model.add(layers.MaxPooling2D(2))
+
+        model.add(layers.SeparableConv2D(64,3,activation='relu'))
+        model.add(layers.SeparableConv2D(128,3,activation='relu'))
+        model.add(layers.GlobalAveragePooling2D())
+
+        model.add(layers.Dense(32, activation='relu'))
+        model.add(layers.Dense(5, activation='softmax'))
+
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
     return model
 
 
-train_buildings, train_labels = read_data(fpath1, MODEL_NAME)
-test_buildings, test_labels = read_data(fpath2, MODEL_NAME)
+# train_buildings, train_labels = read_data(fpath1, MODEL_NAME)
+# test_buildings, test_labels = read_data(fpath2, MODEL_NAME)
 
-key = 1
-x = train_buildings[key]
-y = train_labels[key]
-train_val_split = int(len(x))  # mask index array
-# train
-x_train = np.array(x[:train_val_split])
-y_train = np.array(y[:train_val_split])
-# validation
-# x_val = np.array(x[train_val_split:])
-# y_val = np.array(y[train_val_split:])
-# test
-x_test = np.array(test_buildings[key])
-y_test = np.array(test_labels[key])
+# # key = 1
+# # x = train_buildings[key]
+# # y = train_labels[key]
+# # train_val_split = int(0.9 * len(x))  # mask index array
+# # # train
+# # x_train = np.array(x[:train_val_split])
+# # y_train = np.array(y[:train_val_split])
+# # # validation
+# # x_val = np.array(x[train_val_split:])
+# # y_val = np.array(y[train_val_split:])
+# # # test
+# # x_test = np.array(test_buildings[key])
+# # y_test = np.array(test_labels[key])
 
 # model = nn_model(MODEL_NAME)
-# history = model.fit(x_train, y_train, epochs=20, batch_size=64)
+# history = model.fit(x_train, y_train, epochs=20, batch_size=512)
 # test_loss, test_acc = model.evaluate(x_test, y_test)
 # print('Test accuracy:', test_acc)
 # predictions = model.predict(x_test)
 
 # correct_prediction = 0
 # floor = [0,0,0,0,0]
+# count_0 = 0
+# count_2 = 0
 
 # for i in range(1, len(x_test)):
 #     pred = np.argmax(predictions[-i])
@@ -175,6 +210,11 @@ y_test = np.array(test_labels[key])
 #         correct_prediction += 1
 #     else:
 #         floor[actl] += 1
+#         if (actl == 1):
+#             if (pred == 0):
+#                 count_0 += 1
+#             if (pred == 2):
+#                 count_2 += 1
 
 # s = len(x_test)
 # accuracy = 100 * correct_prediction / s
@@ -182,9 +222,12 @@ y_test = np.array(test_labels[key])
 # print("For ", s, " test data:")
 # print("floor accuracy: ", accuracy, "%")
 # print("Total mistakes: ", sum(floor), " : ", floor)
+# print("1 count as 0: ", count_0);
+# print("1 count as 2: ", count_2)
 
-for item in sub_floors:
-    item = np.array(item)
-    print(item.shape)
+
+# for item in sub_floors:
+#     item = np.array(item)
+#     print(item.shape)
 
 
