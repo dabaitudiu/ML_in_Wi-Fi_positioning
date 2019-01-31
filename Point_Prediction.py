95238def26a2472ef37af0fe27a43ae35b9653be
dabaitudiu <bigbroase@163.com>
2019-02-01 01:52:51 +0800

    point meter test version-1.0

--- Point_Prediction.py
+++ Point_Prediction.py
@@ -10,9 +10,18 @@ from keras import optimizers
 from sklearn.feature_selection import SelectKBest 
 from sklearn.feature_selection import chi2, f_classif, mutual_info_classif
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+import warnings
 
 fpath1 = "trainingData2.csv"
 fpath2 = "validationData2.csv"
+
+point_data = 'final_data.csv'
+point_data_df = pd.read_csv(point_data, header=0)
+point_data_df.columns = ['BF','Point','Latitude','Altitude']
+
+warnings.simplefilter("ignore", category=UserWarning)
+warnings.simplefilter("ignore", category=RuntimeWarning)
+
 
 def one_hot_conversion(a,b):
     m = np.zeros(3)
@@ -47,7 +56,8 @@ def read_data(fpath):
     for i in idx:
         row = rows[i]
         key = str(int(float(row[523]))) + "-" +  str(int(float(row[522])))
-        label = str(int(float(row[524]))) + "_" +  str(int(float(row[525])))
+        label = str(int(float(row[524]))) + "-" +  str(int(float(row[525])))
+        cord = [row[520], row[521]]
         signals = (sub_rows[i] + 110) * 255 / 110
         if key not in pos.keys():
             pos[key] = []
@@ -58,13 +68,13 @@ def read_data(fpath):
         label = count_label_number[key].index(label)
         pos[key].append(signals)
         labels[key].append(label)
+    return pos,labels,count_label_number
 
-    return pos,labels
-
-train_keys, train_labels = read_data(fpath1)
+train_keys, train_labels, z0 = read_data(fpath1)
 # test_keys, test_labels = read_data(fpath2, MODEL_NAME)
 
 results = {}
+meters_results = {}
 for key in train_keys.keys():
     x = train_keys[key]
     y = train_labels[key]
@@ -72,6 +82,9 @@ for key in train_keys.keys():
     # train
     x_train = np.array(x[:train_val_split])
     y_train = np.array(y[:train_val_split])
+
+    x_train_0 = np.array(x)
+    y_train_0 = np.array(y)
     # validation
     x_test = np.array(x[train_val_split:])
     y_test = np.array(y[train_val_split:])
@@ -80,20 +93,53 @@ for key in train_keys.keys():
     # y_test = np.array(test_labels[key])
 
     selector= SelectKBest(score_func= f_classif, k=200)
-    selector.fit(x_train, y_train)
-    select_X = selector.transform(x_train)
+    selector.fit(x_train_0, y_train_0)
+    select_X = selector.transform(x_train_0)
     select_testX = selector.transform(x_test)
 
     clf = RandomForestClassifier(n_estimators=500)
-    clf = clf.fit(select_X, y_train)
+    clf = clf.fit(select_X, y_train_0)
     # a = clf.predict_proba(select_testX)
     pred = clf.predict(select_testX)
 
     count = 0
     for i in range(len(select_testX)):
+        z = z0[key]
+        # if (key == '2-4') or (key == '1-1'):
+        #     continue
+        condition1 = point_data_df['BF'] == key
+        condition2 = point_data_df['Point'] == z[pred[i]]
+        condition3 = condition1 & condition2 
+        # print("Predicting ", key, z[pred[i]])
+        pred_len = len(point_data_df[condition3].values)
+        if (pred_len == 0):
+            print("Error at ", key, z[pred[i]])
+        pred_data_df = point_data_df[condition3].values[0]
+        pred_altitude = pred_data_df[2]
+        pred_longitude = pred_data_df[3]
+        # print("predicted bf", key, "point", z[pred[i]], "at", pred_altitude, " , ", pred_longitude)
+
+        condition1 = point_data_df['BF'] == key
+        condition2 = point_data_df['Point'] == z[y_test[i]]
+        condition3 = condition1 & condition2 
+        test_data_df = point_data_df[condition3].values[0]
+        test_altitude = test_data_df[2]
+        test_longitude = test_data_df[3]
+        # print("Actual point ", z[y_test[i]], "at", test_altitude, " , ", test_longitude)
+        # print('-' * 50)
+        
+        difference = np.square((pred_altitude - test_altitude)) + np.square((pred_longitude - test_longitude))
+        # if (z[y_test[i]] != z[pred[i]]):
+        #     print(key, z[pred[i]], z[y_test[i]], pred_altitude, test_altitude, pred_longitude, test_longitude, difference)
+
+        new_key = key + "-" + z[y_test[i]]
+        if new_key not in meters_results.keys():
+            meters_results[new_key] = []
+        meters_results[new_key].append(difference)
+
         if (pred[i] == y_test[i]):
             count += 1
-    print(count / len(select_testX))
+    # print(key,count / len(select_testX))
 
     if key not in results.keys():
         results[key] = []
@@ -101,3 +147,12 @@ for key in train_keys.keys():
 
 for key in results.keys():
     print(key," : ", results[key])
+
+total_sum = 0
+for item in meters_results.keys():
+    a = meters_results[item]
+    if len(a) != 0:
+        a_result = np.sum(a) / len(a)
+        print(item, " : ", a_result)
+        total_sum += a_result
+print(total_sum / len(x_train_0))
